7ef637cbee5507217dac76cfe6335016055151d7
dhrinch <18438000+dhrinch@users.noreply.github.com>
2024-07-08 13:06:29 -0400

    Update about.component.ts

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, } from '@angular/core';
-import { AboutService } from 'src/app/services/about/about.service';
+import { AboutService } from '../../services/about/about.service';
 
 @Component({
   selector: 'app-about',
